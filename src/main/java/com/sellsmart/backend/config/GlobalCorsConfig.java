c49c5ff8ef538a687e7a00db1d939b6b3f8acda7
hrittijab <hrittija2001@gmail.com>
2025-05-31 12:39:31 -0600

    For deploying

--- src/main/java/com/sellsmart/backend/config/GlobalCorsConfig.java
+++ src/main/java/com/sellsmart/backend/config/GlobalCorsConfig.java
@@ -14,10 +14,7 @@ public class GlobalCorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins(
-                            "https://sellsmart2025.netlify.app",
-                            "http://localhost:3000"
-                        )
+                        .allowedOrigins("*") 
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
